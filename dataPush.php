26f78a0e003fb4c6c3fa1245ba022dc259eae4c6
Sanchayan <sanchayan.sarkar1997@gmail.com>
2017-08-12 01:25:50 +0530

    new

--- dataPush.php
+++ dataPush.php
@@ -1,7 +1,7 @@
 <?php
 $conn= mysqli_connect("localhost", "root", "", "butterflyeffect")
                 or die(mysqli_error($conn));
-$sql = "INSERT INTO butterflyeffect.details(id,firstname, lastname, email,phone)
+$sql = "INSERT INTO butterflyeffect.details(id,firstname,lastname,email,phone)
 VALUES ('user002','John', 'Doe', 'john@example.com','987654210')";
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
